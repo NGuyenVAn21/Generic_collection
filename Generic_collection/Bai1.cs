191c1297bd14fc82b941aa9f31871757d8f6cab9
NGuyenVAn21 <dinhvanparkour@gmail.com>
2026-03-18 21:09:03 +0700

    Bai 1: Tao List<int> va them 5 phan tu

---
+++ Generic_collection/Bai1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Ho ten: Nguyen Dinh Van
+// MSSV: 23115053122248
+// Lop: 225LTC02
+
+class Bai1
+{
+    public static void Run()
+    {
+        List<int> danhSach = new List<int>();
+
+        danhSach.Add(10);
+        danhSach.Add(25);
+        danhSach.Add(7);
+        danhSach.Add(42);
+        danhSach.Add(33);
+
+        Console.WriteLine("Ho ten: Nguyen Dinh Van");
+        Console.WriteLine("MSSV: 23115053122248");
+        Console.WriteLine("Lop: 225LTC02");
+        Console.WriteLine("========================");
+        Console.WriteLine("Danh sach cac so nguyen:");
+
+        foreach (int so in danhSach)
+        {
+            Console.WriteLine(so);
+        }
+    }
+}
