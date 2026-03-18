8de38f94644872f882d6d79772369e2e12f6eda1
NGuyenVAn21 <dinhvanparkour@gmail.com>
2026-03-18 21:42:04 +0700

    Bai 10: Tao Bai10.cs nhap danh sach so nguyen

---
+++ Generic_collection/Bai10.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Ho ten: Nguyen Dinh Van
+// MSSV: 23115053122248
+// Lop: 225LTC02
+// Bai 10: Tim phan tu nho nhat trong List<int>
+
+class Bai10
+{
+    public static void Run()
+    {
+        List<int> danhSach = new List<int>();
+        int n;
+
+        Console.WriteLine("Ho ten: Nguyen Dinh Van");
+        Console.WriteLine("MSSV: 23115053122248");
+        Console.WriteLine("Lop: 225LTC02");
+        Console.WriteLine("========================");
+        Console.WriteLine("Bai 10: Tim phan tu nho nhat");
+        Console.WriteLine("========================");
+
+        Console.Write("Nhap so luong phan tu: ");
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu thu {i + 1}: ");
+            int x = int.Parse(Console.ReadLine());
+            danhSach.Add(x);
+        }
+
+        int min = danhSach[0];
+        foreach (int so in danhSach)
+        {
+            if (so < min)
+                min = so;
+        }
+
+        Console.WriteLine("Danh sach: " + string.Join(", ", danhSach));
+        Console.WriteLine($"So nho nhat: {min}");
+    }
+}
