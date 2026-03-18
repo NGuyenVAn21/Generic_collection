f632b6b7f99d9d55f2418cf22894b4618c717745
NGuyenVAn21 <dinhvanparkour@gmail.com>
2026-03-18 21:16:31 +0700

    Bai 2: Tao Bai2.cs nhap so lieu vao List<int>

--- Generic_collection/Bai2.cs
+++ Generic_collection/Bai2.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace Generic_collection
+// Ho ten: Nguyen Dinh Van
+// MSSV: 23115053122248
+// Lop: 225LTC02
+// Bai 2: Tinh tong cac phan tu trong List<int>
+
+class Bai2
 {
-    internal class Bai2
+    public static void Run()
     {
+        List<int> danhSach = new List<int>();
+        int n;
+
+        Console.WriteLine("Ho ten: Nguyen Dinh Van");
+        Console.WriteLine("MSSV: 23115053122248");
+        Console.WriteLine("Lop: 225LTC02");
+        Console.WriteLine("========================");
+        Console.WriteLine("Bai 2: Tinh tong cac phan tu");
+        Console.WriteLine("========================");
+
+        Console.Write("Nhap so luong phan tu: ");
+        n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu thu {i + 1}: ");
+            int x = int.Parse(Console.ReadLine());
+            danhSach.Add(x);
+        }
+
+        int tong = 0;
+        foreach (int so in danhSach)
+            tong += so;
+
+        Console.WriteLine($"Tong cac phan tu: {tong}");
     }
 }
